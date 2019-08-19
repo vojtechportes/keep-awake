f1b3e2284b96a28cd2604c167949563034ad3429
Vojtěch Porteš <vojtech.portes@arbes.com>
2019-08-19 17:57:19 +0200

    Output message, dynamic urls from env variables

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-var http = require("http");
+const http = require("http");
+const express = require('express');
+import http from "https";
+import express from "express";
 
-setInterval(function() {
-    http.get("http://react-query-builder-demo.herokuapp.com");
-    http.get("http://node-keep-awake.herokuapp.com");
+const PATHS = process.env.PATHS.split("\n");
+
+setInterval(() => {
+    PATHS.forEach(url => {
+      http.get(url, () => {
+        console.log(`${url} called at ${new Date().toISOString()}`)
+      });
+    })
 }, 60000);
+
+
+const app = express()
+ 
+app.get('/', () => {
+  res.send('Up and running!')
+})
+ 
+app.listen(3000)
